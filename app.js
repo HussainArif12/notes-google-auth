13126c080b609cdd53e5f8958c22fe9c1ebebe2b
Hussain <hussain@pop-os.localdomain>
2020-11-11 22:47:23 +0500

    New change

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-const app = require('express')();
+const express = require('express');
+const exphbs = require('express-handlebars');
 const dotenv = require('dotenv');
+const path = require('path');
 
+const app = express();
 const PORT = 3000;
+
+//handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main' , 'extname' : '.hbs'}));
+app.set('view engine', '.hbs');
+
+//static folder
+app.use(express.static(path.join(__dirname,'public')));
+
+
 //load dotenv config.
 dotenv.config();
 app.listen(PORT , console.log('LIstening at port 3000'));
