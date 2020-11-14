c5c8fb68620df02cfb6894c83985a3ded29f7782
Hussain <hussain@pop-os.localdomain>
2020-11-14 22:04:14 +0500

    Added passport.js

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const dotenv = require('dotenv');
-const path = require('path');
+const passport = require('passport');
+const session  = require('express-session');
 const connectDB = require('./config/db')
+
 const app = express();
 const PORT = 3000;
+
+//passport config
+require('./config/passport')(passport);
 
 //load dotenv config.
 dotenv.config();    
@@ -12,6 +17,17 @@ connectDB();
 //handlebars
 app.engine('.hbs', exphbs({defaultLayout: 'main' , 'extname' : '.hbs'}));
 app.set('view engine', '.hbs');
+
+//Express sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //routes
 app.use('/',require('./routes/index'));
---
+++ config/passport.js
@@ -0,0 +1,24 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const User = require('../models/User');
+
+module.exports = function(passport){ 
+    passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback"
+      },
+      async(accessToken, refreshToken, profile, done)=> {
+       console.log(profile);
+      }
+    ));
+    passport.serializeUser((user, done)=> {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser((id, done)=> {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
+}
