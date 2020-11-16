ee1b33c340df482a6b558ae69a69ec51818918ed
Hussain <hussain@pop-os.localdomain>
2020-11-16 18:22:20 +0500

    Added auth middleware

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express=  require('express');
+const {ensureAuth,ensureGuest} = require('../middleware/auth')
 const router = express.Router();
 
-router.get('/',(req,res)=>{
+router.get('/',ensureGuest,(req,res)=>{
     res.render('login',
     {
         layout: 'login'
     });
 })
 
-router.get('/dashboard',(req,res)=>{
+router.get('/dashboard',ensureAuth,(req,res)=>{
     res.render('dashboard')
 })
 
